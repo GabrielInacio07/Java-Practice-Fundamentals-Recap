d8bd3ece128a7b342b2a138982ed9b36ada0598f
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-01 12:53:32 -0300

    Criando o OBJETO Pessoa

---
+++ revisar-variaveis/src/Pessoa.java
@@ -0,0 +1,56 @@
+public class Pessoa {
+
+    private String nome;
+    private double altura;
+    private double peso;
+    private char sexo;
+
+    public Pessoa(String nome, double altura, double peso, char sexo) {
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                ", sexo=" + sexo +
+                '}';
+    }
+}
