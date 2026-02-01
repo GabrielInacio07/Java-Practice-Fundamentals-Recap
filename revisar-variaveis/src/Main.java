95875f5df8f9729092169f3e90108ebc175a25a9
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-01 13:49:54 -0300

    Terceiro Desafio do módulo de revisão (Somar valores double exibir seu valor com casas decimais, e arredondar conforme métricas mátematicas)

--- revisar-variaveis/src/Main.java
+++ revisar-variaveis/src/Main.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         System.out.println("Revisão de TIPOS/VARIÁVEIS");
-        System.out.println("Exercício fazendo CASTING DE VALORES");
+        System.out.println("Exercício fazendo arredondamento DE VALORES");
         Scanner input = new Scanner(System.in);
-        double estoque = 0;
-
-        System.out.println("Digite a quantidade de estoque a inserir");
-        estoque = Double.parseDouble(input.nextLine());
-
-        System.out.println("Valor passado pelo usuário: " + estoque);
-        System.out.println("Valor incrementado no estoque em inteiro: " + (int) estoque);
 
 
+        double nota = 0;
+        double nota2 = 0;
+        double nota3 = 0;
+        double resultado = 0;
+
+        System.out.println("=============================");
+        System.out.println("Digite a sua [PRIMEIRA] nota");
+        nota = Double.parseDouble(input.nextLine());
+        System.out.println("Digite a sua [SEGUNDA] nota");
+        nota2 = Double.parseDouble(input.nextLine());
+        System.out.println("Digite a sua [TERCEIRA] nota");
+        nota3 = Double.parseDouble(input.nextLine());
+
+        resultado = (nota + nota2 + nota3) / 3;
+
+        System.out.println("A média das notas: " + resultado);
+        System.out.println("Média arredondada: " + Math.round(resultado));
+
+        input.close();
     }
 }
