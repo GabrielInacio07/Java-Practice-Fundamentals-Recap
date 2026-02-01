c17c0d86f498a599288a0b8616fb08854eb78505
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-01 13:36:42 -0300

    Primeiro Desafio do módulo de revisão

---
+++ revisar-variaveis/src/D01/PessoaEstudante.java
@@ -0,0 +1,42 @@
+package D01;
+
+public class PessoaEstudante {
+
+    private String nome;
+    private int idade;
+    private double altura;
+    private boolean estudante;
+
+    public PessoaEstudante(String nome, int idade, double altura, boolean estudante) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.estudante = estudante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public boolean isEstudante() {
+        return estudante;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaEstudante{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                ", estudante=" + estudante +
+                '}';
+    }
+}
--- revisar-variaveis/src/Main.java
+++ revisar-variaveis/src/Main.java
@@ -1,15 +1,19 @@
+import D01.PessoaEstudante;
+
 class Main{
     public static void main(String[] args) {
     System.out.println("Revisão de TIPOS/VARIÁVEIS");
 
-        int quantidadeDePassos = 500;
+        PessoaEstudante pessoa_estudante = new PessoaEstudante("João",21,1.90,false);
 
-        Pessoa people = new Pessoa("Gabriel",1.85,95.0,'M',300);
+        //Verificando escopo
 
-        if(people.getQuantidadeDePassos() < 500){
-            System.out.println(people.getNome() + " você precisa aumentar a quantidade de passos");
-        }else {
-            System.out.println(people.getNome() + " você está dentro da meta exigida");
+        if (pessoa_estudante.isEstudante()){
+            System.out.println(pessoa_estudante.getNome() + " é um estudante");
+            System.out.println(pessoa_estudante.toString());
+        }else{
+            System.out.println(pessoa_estudante.getNome() + " não é um estudante");
         }
+
     }
 }
